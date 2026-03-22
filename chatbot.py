2d1325d5a8110a990e2fc4da28837895ef03d924
slick-v <ronn.proff@gmail.com>
2026-03-22 17:58:20 +0530

    toolbot

--- chatbot.py
+++ chatbot.py
@@ -13,13 +13,24 @@ llm = ChatOllama(model="llama3")
 # memory
 memory = ConversationBufferMemory(return_messages=True)
 
+# caluculator tool
+def caluculator(expression: str):
+    try:
+        result = eval(expression)
+        return str(result)
+    except:
+        return "Invalid math expression"
+
 
 # define prompt
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "You Are a helpful Ai Assisitant, Explain Clearly in simple Words"),
+    ("system", "You Are a helpful Ai Assisitant, "
+    "if  user asks math question, use the caluculator provided,"
+    "expalin the reson clearly"),
 
     # memory will be inserted here
     MessagesPlaceholder(variable_name="history"),
+
     ("human" , "{question}")
 
 ])
@@ -35,6 +46,18 @@ while True:
 
     if user_input.lower() == "exit":
         break
+
+    # simple logic to detect math expression
+    if any(op in user_input for op in ["+" , "-", "*" , "/"]):
+
+        tool_result = caluculator(user_input)
+
+        user_input =  f"""
+        user question: {user_input}
+        calucualtor result: {tool_result}
+        explain the result
+
+    """
 
     # load history from memory
     history = memory.load_memory_variables({})["history"]
